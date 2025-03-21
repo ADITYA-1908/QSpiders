cf48d3e54557e77bffc5f6d6e0939bb8c53d028c
ADITYA BASANTIA <adityabasantia729@gmail.com>
2025-03-21 17:59:43 +0530

    javaScript

---
+++ JavaScript/Looping/looping.js
@@ -0,0 +1,14 @@
+//for loop
+
+//while loop
+var i = 1;
+while (i < 5) {
+  console.log(i);
+  i++;
+}
+
+var arr = ["adi", "adi_subham", "aditya"];
+
+for (var i = 0; (i = length - 1); i++) {
+  console.log(arr[i]);
+}
