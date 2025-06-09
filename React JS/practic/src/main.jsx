3029191deb2a53ae4093927cdb90529da5cb987c
ADITYA BASANTIA <adityabasantia729@gmail.com>
2025-06-09 15:35:33 +0530

    Routes

--- React JS/practic/src/main.jsx
+++ React JS/practic/src/main.jsx
@@ -1,10 +1,7 @@
-// main.jsx or index.jsx
-
-import React from "react"; // ✅ this line ensures JSX works
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
