cd6f24955dda6f3214ce0c098be6cb78fc497883
ADITYA BASANTIA <adityabasantia729@gmail.com>
2025-07-28 16:49:27 +0530

    PostMen

--- Backend/Express JS/server.js
+++ Backend/Express JS/server.js
@@ -1,24 +1,47 @@
-const express = require("express")
+const express = require("express");
 const app = express();
+app.use(express.json());
 
-const obj = {
-    userName: "Aditya",
-    age: 20
-}
+const userDetails = [
+    {
+        id: 1,
+        userName: "aditya",
+        email: "aditya@gmail.com",
+        password: "adi123"
+    },
+    {
+        id: 2,
+        userName: "anurag",
+        email: "anurag@gmail.com",
+        password: "anu123"
+    }
+];
 
-const data = {
-    userName: "Anurag",
-    age: 22
-}
+// Get all users
+app.get("/userdetails", (req, res) => {
+    res.json({ userDetails });
+});
 
-app.get("/", (req, res) => {
-    res.json({ obj })
-})
+// Register new user
+app.post("/register", (req, res) => {
+    const newUser = req.body;
+    userDetails.push(newUser);
+    res.json({ message: "Register Successfully" });
+});
 
-app.get("/test", (req, res) => {
-    res.json({ data })
-})
+// Get user by ID
+app.get("/userdetails/:id", (req, res) => {
+    const { id } = req.params;
+    const numericId = parseInt(id);
+    const userData = userDetails.find(user => user.id === numericId);
+
+    if (userData) {
+        res.json({ user: userData });
+    } else {
+        res.status(404).json({ message: "User not found" });
+    }
+});
 
 app.listen(8080, () => {
-    console.log("server started at http://localhost:8080/ ")
-})
+    console.log("http://localhost:8080");
+});
