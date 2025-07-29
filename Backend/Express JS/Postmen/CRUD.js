67ead1a6c158e7771ef09f89b8cd1019cb6f0961
ADITYA BASANTIA <adityabasantia729@gmail.com>
2025-07-29 20:24:33 +0530

    CRUD

---
+++ Backend/Express JS/Postmen/CRUD.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+let userDetails = [
+
+];
+
+// Register new user
+app.post("/register", (req, res) => {
+    const newUser = req.body;
+    userDetails.push(newUser);
+    res.json({ message: "Registered Successfully", user: newUser });
+});
+
+// Login 
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    const matchedUsers = userDetails.filter(user => user.email === email && user.password === password);
+
+    if (matchedUsers.length > 0) {
+        res.json({ message: "Login Successful", user: matchedUsers });
+    } else {
+        res.json({ message: "Invalid email or password" });
+    }
+});
+
+// Get all users
+app.get("/userdetails", (req, res) => {
+    res.json({ users: userDetails });
+});
+
+// Get user by ID
+app.get("/userdetail/:id", (req, res) => {
+    const { id } = req.params;
+    const numericId = parseInt(id);
+    const userData = userDetails.filter(user => user.id === numericId);
+
+    if (userData) {
+        res.json({ user: userData });
+    } else {
+        res.json({ message: "User not found" });
+    }
+});
+
+
+// Delete user by ID 
+app.delete("/deleteUser/:id", (req, res) => {
+    const { id } = req.params;
+    const numericId = parseInt(id);
+    const user = userDetails.some((e) => e.id === id)
+    if (user >= 0) {
+        userDetails = userDetails.filter(user => user.id !== numericId);
+        res.json({ message: "User deleted successfully" });
+    } else {
+        res.json({ message: "User not found" });
+    }
+});
+
+
+// Update user by ID (PUT)
+app.put("/updateUser/:id", (req, res) => {
+    const { id } = req.params;
+    const numericId = parseInt(id);
+    const newData = req.body;
+
+    const userIndex = userDetails.findIndex(user => user.id === numericId);
+
+    if (userIndex >= 0) {
+        userDetails[userIndex] = newData;
+
+        res.json({
+            message: "User updated successfully",
+            updatedUser: userDetails[userIndex]
+        });
+    } else {
+        res.json({ message: "User not found" });
+    }
+});
+
+
+
+app.listen(8080, () => {
+    console.log("http://localhost:8080");
+});
--- Backend/Express JS/server.js
+++ Backend/Express JS/server.js
@@ -1,47 +1,6 @@
-const express = require("express");
-const app = express();
-app.use(express.json());
+//!first demo for postman API (GET POST)
+// require("./Postmen/FirstDemo")
 
-const userDetails = [
-    {
-        id: 1,
-        userName: "aditya",
-        email: "aditya@gmail.com",
-        password: "adi123"
-    },
-    {
-        id: 2,
-        userName: "anurag",
-        email: "anurag@gmail.com",
-        password: "anu123"
-    }
-];
+//!CRUD for postman API
+require("./Postmen/CRUD")
 
-// Get all users
-app.get("/userdetails", (req, res) => {
-    res.json({ userDetails });
-});
-
-// Register new user
-app.post("/register", (req, res) => {
-    const newUser = req.body;
-    userDetails.push(newUser);
-    res.json({ message: "Register Successfully" });
-});
-
-// Get user by ID
-app.get("/userdetails/:id", (req, res) => {
-    const { id } = req.params;
-    const numericId = parseInt(id);
-    const userData = userDetails.find(user => user.id === numericId);
-
-    if (userData) {
-        res.json({ user: userData });
-    } else {
-        res.status(404).json({ message: "User not found" });
-    }
-});
-
-app.listen(8080, () => {
-    console.log("http://localhost:8080");
-});
